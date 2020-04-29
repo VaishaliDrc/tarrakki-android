885381cac44497244ac29dd347c93b48e336134b
jayesh.prakariya <jayesh.prakariya>
2020-04-29 15:58:35 +0530

    Risk assessment slider-left implemented

--- app/src/main/java/com/tarrakki/module/risk_assesment/AssessmentQFragment.kt
+++ app/src/main/java/com/tarrakki/module/risk_assesment/AssessmentQFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.tarrakki.R
 import com.tarrakki.databinding.FragmentAssessmentQBinding
+import com.tarrakki.databinding.RowRiskAssessmentSliderItemStartBinding
+import kotlinx.android.synthetic.main.fragment_assessment_q.*
 import org.supportcompact.CoreFragment
+import org.supportcompact.adapters.setUpRecyclerView
 
 /**
  * A simple [Fragment] subclass.
@@ -30,7 +33,18 @@ class AssessmentQFragment : CoreFragment<AssessmentQVM, FragmentAssessmentQBindi
     }
 
     override fun createReference() {
-
+        rvQuestions?.setUpRecyclerView(R.layout.row_risk_assessment_slider_item_start, getViewModel().sliderQuestions) { item: SliderItem, binder: RowRiskAssessmentSliderItemStartBinding, position: Int ->
+            binder.item = item
+            binder.executePendingBindings()
+            binder.tvTitle.setOnClickListener {
+                //getViewModel().sliderQuestions.forEach { it.isSelected = false }
+                getViewModel().sliderQuestions.forEachIndexed { index, it ->
+                    it.isSelected = false
+                    it.isMovedOver = index < position
+                }
+                getViewModel().sliderQuestions[position].isSelected = true
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/com/tarrakki/module/risk_assesment/AssessmentQVM.kt
+++ app/src/main/java/com/tarrakki/module/risk_assesment/AssessmentQVM.kt
@@ -1,6 +1,36 @@
 package com.tarrakki.module.risk_assesment
 
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import androidx.databinding.library.baseAdapters.BR
 import org.supportcompact.FragmentViewModel
 
 class AssessmentQVM : FragmentViewModel() {
+
+    val sliderQuestions = arrayListOf<SliderItem>()
+
+    init {
+        sliderQuestions.add(SliderItem("Below 5000"))
+        sliderQuestions.add(SliderItem("5000 - 15000"))
+        sliderQuestions.add(SliderItem("15000 - 30000"))
+        sliderQuestions.add(SliderItem("30000 - 60000"))
+        sliderQuestions.add(SliderItem("Above 60000"))
+    }
+}
+
+data class SliderItem(val label: String, private val selected: Boolean = false) : BaseObservable() {
+
+    @get:Bindable
+    var isSelected: Boolean = selected
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.selected)
+        }
+
+    @get:Bindable
+    var isMovedOver: Boolean = false
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.movedOver)
+        }
 }
