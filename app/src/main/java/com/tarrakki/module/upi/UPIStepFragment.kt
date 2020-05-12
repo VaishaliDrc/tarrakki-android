2d00ee3b88658525492dd6d5368ff5f19ded3bc5
parth.p <parth.p@drcsystems.com>
2020-05-12 18:58:26 +0530

    [IMP]#111 - Implement Payment status api - Check payment status on every 5 sec and redirect to transaction confirm screen id getting success response

--- app/src/main/java/com/tarrakki/module/upi/UPIStepFragment.kt
+++ app/src/main/java/com/tarrakki/module/upi/UPIStepFragment.kt
@@ -1,12 +1,25 @@
 package com.tarrakki.module.upi
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
+import androidx.lifecycle.Observer
 import com.tarrakki.R
-import com.tarrakki.databinding.FragmentPaymentModeBinding
 import com.tarrakki.databinding.FragmentUpiStepBinding
+import com.tarrakki.module.netbanking.NET_BANKING_PAGE
+import com.tarrakki.module.paymentmode.ISFROMTRANSACTIONMODE
+import com.tarrakki.module.paymentmode.SUCCESSTRANSACTION
+import com.tarrakki.module.paymentmode.SUCCESS_ORDERS
+import com.tarrakki.module.paymentmode.TRANSACTION_IDS
+import com.tarrakki.module.transactionConfirm.TransactionConfirmFragment
+import org.greenrobot.eventbus.EventBus
 import org.supportcompact.CoreFragment
+import org.supportcompact.events.Event
+import org.supportcompact.ktx.DISMISS_PROGRESS
+import org.supportcompact.ktx.simpleAlert
+import org.supportcompact.ktx.startFragment
 
+const val UPI_PAGE = "upi_page"
 
 class UPIStepFragment : CoreFragment<UPIStepVM, FragmentUpiStepBinding>() {
 
@@ -15,15 +28,54 @@ class UPIStepFragment : CoreFragment<UPIStepVM, FragmentUpiStepBinding>() {
     override val title: String
         get() = getString(R.string.upi_payment)
 
+    lateinit var countdown_timer: CountDownTimer
+
 
     override fun getLayout(): Int {
         return R.layout.fragment_upi_step
     }
 
     override fun createReference() {
+        countdown_timer = object : CountDownTimer(300000, 5000) {
+            override fun onTick(millisUntilFinished: Long) {
+                checkstatusApi()
+            }
+
+            override fun onFinish() {
+                context?.simpleAlert(getString(R.string.something_went_wrong_please_try_again)) {
+                    countdown_timer.cancel()
+                    //                    redirectTo()
+                }
+            }
+        }.start()
         coreActivityVM?.footerVisibility?.set(View.GONE)
+        getViewModel().transaction_ids = arguments?.getString(TRANSACTION_IDS)!!
 
     }
+
+    private fun redirectTo() {
+        val bundle = Bundle().apply {
+            arguments?.getString(SUCCESS_ORDERS)?.let { it1 -> putString(SUCCESS_ORDERS, it1) }
+            arguments?.getString(SUCCESSTRANSACTION)?.let { it1 -> putString(SUCCESSTRANSACTION, it1) }
+            arguments?.getBoolean(ISFROMTRANSACTIONMODE)?.let { it1 -> putBoolean(ISFROMTRANSACTIONMODE, it1) }
+            putBoolean(NET_BANKING_PAGE, true)
+        }
+        startFragment(TransactionConfirmFragment.newInstance(bundle), R.id.frmContainer)
+    }
+
+    fun checkstatusApi() {
+        getViewModel().checkPaymentStatus().observe(this, Observer {
+            it?.data?.let {
+                if (it.payment) {
+                    EventBus.getDefault().post(DISMISS_PROGRESS)
+                    countdown_timer.cancel()
+                    redirectTo()
+                }
+            }
+        })
+
+    }
+
 
     override fun createViewModel(): Class<out UPIStepVM> {
         return UPIStepVM::class.java
--- app/src/main/java/com/tarrakki/module/upi/UPIStepVM.kt
+++ app/src/main/java/com/tarrakki/module/upi/UPIStepVM.kt
@@ -1,8 +1,45 @@
 package com.tarrakki.module.upi
 
+import androidx.lifecycle.MutableLiveData
+import com.tarrakki.App
+import com.tarrakki.api.ApiClient
+import com.tarrakki.api.SingleCallback1
+import com.tarrakki.api.WebserviceBuilder
+import com.tarrakki.api.model.ApiResponse
+import com.tarrakki.api.model.CheckPaymentStatus
+import com.tarrakki.api.model.parseTo
+import com.tarrakki.api.model.printResponse
+import com.tarrakki.api.subscribeToSingle
+import org.greenrobot.eventbus.EventBus
+import org.json.JSONArray
 import org.supportcompact.FragmentViewModel
+import org.supportcompact.ktx.DISMISS_PROGRESS
+import org.supportcompact.ktx.SHOW_PROGRESS
+import org.supportcompact.ktx.getUserId
 
 class UPIStepVM : FragmentViewModel() {
 
+    var transaction_ids: String = ""
+
+    fun checkPaymentStatus(): MutableLiveData<CheckPaymentStatus> {
+        EventBus.getDefault().post(SHOW_PROGRESS)
+        val apiResponse = MutableLiveData<CheckPaymentStatus>()
+        subscribeToSingle(ApiClient.getHeaderClient().create(WebserviceBuilder::class.java).checkPaymentStatus(App.INSTANCE.getUserId(), JSONArray(transaction_ids).toString()),
+                object : SingleCallback1<ApiResponse> {
+                    override fun onSingleSuccess(o: ApiResponse) {
+                        if (o.status?.code == 1) {
+                            o.printResponse()
+                            val apiClient = o.data?.parseTo<CheckPaymentStatus>()
+                            apiResponse.postValue(apiClient)
+                        }
+                    }
+
+                    override fun onFailure(throwable: Throwable) {
+                        EventBus.getDefault().post(DISMISS_PROGRESS)
+                        throwable.printStackTrace()
+                    }
+                })
+        return apiResponse
+    }
 
 }
