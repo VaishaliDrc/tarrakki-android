ccdbce5674463d4ea935b4a617c49bfa00edab78
parth.vyas@drcsystems.com <parth.vyas@drcsystems.com>
2021-01-20 11:07:19 +0530

    - Krupa has implemented social signup. - I have upload App to play store.

--- app/src/main/java/com/tarrakki/module/register/RegisterVM.kt
+++ app/src/main/java/com/tarrakki/module/register/RegisterVM.kt
@@ -15,7 +15,7 @@ import org.supportcompact.ActivityViewModel
 import org.supportcompact.events.ShowError
 import org.supportcompact.ktx.*
 
-class RegisterVM : ActivityViewModel(), SingleCallback<WebserviceBuilder.ApiNames> {
+class RegisterVM : ActivityViewModel() {
 
     val email = ObservableField("")
     val mobile = ObservableField("")
@@ -151,30 +151,11 @@ class RegisterVM : ActivityViewModel(), SingleCallback<WebserviceBuilder.ApiName
                     }
 
                     override fun onFailure(throwable: Throwable, apiNames: WebserviceBuilder.ApiNames) {
-                        this@RegisterVM.onFailure(throwable, apiNames)
+                        EventBus.getDefault().post(DISMISS_PROGRESS)
+                        EventBus.getDefault().post(ShowError("${throwable.message}"))
                     }
                 }
         )
         return onSocialLogin
-    }
-
-    override fun onSingleSuccess(o: Any?, apiNames: WebserviceBuilder.ApiNames) {
-        EventBus.getDefault().post(DISMISS_PROGRESS)
-        if (o is ApiResponse) {
-            o.printResponse()
-            if (o.status?.code == 1) {
-                val data = o.data?.parseTo<LoginResponse>()
-                onLogin.value = data
-            } else {
-                EventBus.getDefault().post(ShowError("${o.status?.message}"))
-            }
-        } else {
-            EventBus.getDefault().post(ShowError(App.INSTANCE.getString(R.string.try_again_to)))
-        }
-    }
-
-    override fun onFailure(throwable: Throwable, apiNames: WebserviceBuilder.ApiNames) {
-        EventBus.getDefault().post(DISMISS_PROGRESS)
-        EventBus.getDefault().post(ShowError("${throwable.message}"))
     }
 }
