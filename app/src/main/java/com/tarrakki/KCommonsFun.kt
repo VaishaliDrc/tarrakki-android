7d3f38a2cc9b4cc68f187e44c6d773316275cfca
Kishan Rajkotiya <kishan.rajkotiya@drcsystems.com>
2021-06-23 09:43:40 +0530

    Small UI changes in Portfolio screen

--- app/src/main/java/com/tarrakki/KCommonsFun.kt
+++ app/src/main/java/com/tarrakki/KCommonsFun.kt
@@ -224,7 +224,7 @@ fun getPortfolioCalculatedIntro(): MutableList<DirectInvestmentFragment.Investme
                     "Investments of one year and above reflect XIRR returns (annualised returns)."))*/
     options.add(DirectInvestmentFragment.InvestmentPortfolioIntro(
             "When does my portfolio get updated?",
-            "Updates that you make to your portfolio today will only be visible here, on your Portfolio Screen, after 10:30 am tomorrow. To check the status of your recent transactions, please navigate to the Transactions screen."))
+            "Updates that you make to your portfolio today will only be visible here, on your Portfolio Screen, after 10:30 am tomorrow. To check the status of your recent transactions, please navigate to the Transactions screen. \n\nNote: Redemptions made outside of the Tarrakki app (ATM or Using your Debit Card) will take T+3 days to update under the portfolio section. You can check the current value of your Tarrakki Zyaada investment at Nippon AMC's Website."))
     return options
 }
 
